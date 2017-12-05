c0cf70c3d6d9653d9d63fe26957156ae22c082a5
whit121 <Taylorwhitlatch112@gmail.com>
2017-12-05 10:38:06 -0500

    initial"

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers/index'
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
